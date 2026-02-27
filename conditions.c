9c1e92df42fc9b9ece389448537f264cd3a01b6d
Kalyx <0xkalyx@proton.me>
2026-02-27 18:28:15 +0000

    conditions downexploring strings

--- conditions.c
+++ conditions.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
-void guessNumber(int guess) {
-    // TODO: write your code here
+void guessNumber(int guess)
+{
+    int secretNumber = 555;
+
+    if (guess < secretNumber)
+    {
+        printf("TOO LOW! guess again. \n");
+    }
+    else if (guess > secretNumber)
+    {
+        printf("TOO HIGH! guess again. \n");
+    }
+    else
+    {
+        printf("CONGRATULATIONS! You guessed the number correctly. \n");
+    }
 }
 
 int main() {
